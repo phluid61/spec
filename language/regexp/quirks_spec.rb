38297f6db991a1cddf3b6ff6de0ad705c0d7e68f
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-07-07 12:44:44 +1000

    yay ruby version inconsistencies

--- language/regexp/quirks_spec.rb
+++ language/regexp/quirks_spec.rb
@@ -17,21 +17,50 @@ describe "regexp quirks" do
       (/\W/ =~ "\uFB06").should == 0  # st
       (/\W/ =~ "\u2133").should == 0  # script capital 'M'
     end
+
     it "doesn't match without a character class" do
       (/\W/i =~ 'SS').should == nil # ß (no cases, no decomposition, ~ 'SS'|'ss')
       (/\W/i =~ 'FF').should == nil # ﬀ (no cases, decomposition = 'f'+'f', ~ 'FF'|'ff')
+      (/\W/i =~ 'M').should == nil  # ℳ (Lu, decomposition = 'M', ~ 'M')
       (/\W/i =~ 'k').should == nil  # K (lc = 'k', decomposition = 'K', ~ 'K'|'k')
     end
-    it "matches with a character class" do
-      (/[\W]/i =~ 'M').should == 0
-      (/[\W]/i =~ 'SS').should == 0
-      (/[\W]/i =~ 'FF').should == 0
-      (/[\W]/i =~ 'k').should == nil #??
-    end
-    it "doesn't make sense with a negated character class" do
-      (/[^\W]/i =~ 'SS').should == 0
-      (/[^\W]/i =~ 'FF').should == 0
-      (/[^\W]/i =~ 'k').should == 0
+
+    if RUBY_VERSION < '2' || RUBY_VERSION >= '2.4'
+      it "doesn't match with a character class" do
+        (/[\W]/i =~ 'SS').should == nil # ß (Ll, no Lu, no decomposition, ~ 'SS'|'ss')
+        (/[\W]/i =~ 'FF').should == nil # ﬀ (Ll, no Lu, decomposition = 'f'+'f', ~ 'FF'|'ff')
+        (/[\W]/i =~ 'M' ).should == nil # ℳ (Lu, no Ll, decomposition = 'M', ~ 'M')
+        (/[\W]/i =~ 'k').should == nil  # K (Lu, Ll = 'k', decomposition = 'K', ~ 'K'|'k')
+      end
+      it "doesn't make sense with a negated character class" do
+        (/[^\W]/i =~ 'SS').should == 0
+        (/[^\W]/i =~ 'FF').should == 0
+        (/[^\W]/i =~ 'k').should == 0
+      end
+    elsif RUBY_VERSION < '2.2'
+      it "sometimes matches with a character class" do
+        (/[\W]/i =~ 'SS').should == 0   # ß (Ll, no Lu, no decomposition, ~ 'SS'|'ss')
+        (/[\W]/i =~ 'FF').should == 0   # ﬀ (Ll, no Lu, decomposition = 'f'+'f', ~ 'FF'|'ff')
+        (/[\W]/i =~ 'M' ).should == nil # ℳ (Lu, no Ll, decomposition = 'M', ~ 'M')
+        (/[\W]/i =~ 'k').should == 0    # K (Lu, Ll = 'k', decomposition = 'K', ~ 'K'|'k')
+      end
+      it "sometimes doesn't make sense with a negated character class" do
+        (/[^\W]/i =~ 'SS').should == nil
+        (/[^\W]/i =~ 'FF').should == 0
+        (/[^\W]/i =~ 'k').should == nil
+      end
+    else
+      it "sommetimes matches with a character class" do
+        (/[\W]/i =~ 'SS').should == 0   # ß (Ll, no Lu, no decomposition, ~ 'SS'|'ss')
+        (/[\W]/i =~ 'FF').should == 0   # ﬀ (Ll, no Lu, decomposition = 'f'+'f', ~ 'FF'|'ff')
+        (/[\W]/i =~ 'M' ).should == nil # ℳ (Lu, no Ll, decomposition = 'M', ~ 'M')
+        (/[\W]/i =~ 'k').should == nil  # K (Lu, Ll = 'k', decomposition = 'K', ~ 'K'|'k')
+      end
+      it "doesn't make sense with a negated character class" do
+        (/[^\W]/i =~ 'SS').should == 0
+        (/[^\W]/i =~ 'FF').should == 0
+        (/[^\W]/i =~ 'k').should == 0
+      end
     end
 
     # 'fL' =~ U+FB02 LATIN SMALL LIGATURE FL
